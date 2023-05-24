d2b434276364d594cf06587f69e8d61bb835ab75
SOUYKING <104052247+SOUYKING@users.noreply.github.com>
2023-05-24 15:28:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var copyButton = document.getElementById('copyButton');
-    var textToCopy = document.querySelector('p').innerText;
-  
-    copyButton.addEventListener('click', function() {
-      navigator.clipboard.writeText(textToCopy)
-        .then(function() {
-          alert('Text copied to clipboard!');
-        })
-        .catch(function() {
-          alert('Error copying text to clipboard.');
-        });
-    });
-  });
-  
+// Copy text when copyButton1 is clicked
+document.getElementById("copyButton1").addEventListener("click", function() {
+  var textToCopy = document.getElementById("textToCopy1").innerText;
+  copyTextToClipboard(textToCopy);
+});
+
+// Copy text when copyButton2 is clicked
+document.getElementById("copyButton2").addEventListener("click", function() {
+  var textToCopy = document.getElementById("textToCopy2").innerText;
+  copyTextToClipboard(textToCopy);
+});
+
+// Copy text when copyButton3 is clicked
+document.getElementById("copyButton3").addEventListener("click", function() {
+  var textToCopy = document.getElementById("textToCopy3").innerText;
+  copyTextToClipboard(textToCopy);
+});
+
+// Function to copy text to clipboard
+function copyTextToClipboard(text) {
+  var dummyElement = document.createElement("textarea");
+  document.body.appendChild(dummyElement);
+  dummyElement.value = text;
+  dummyElement.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummyElement);
+  alert("Text copied to clipboard: " + text);
+}
